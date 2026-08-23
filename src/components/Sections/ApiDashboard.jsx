794361b55a45745d954ed35b73d326d4a2ff6d91
ShozabRizvi <rshozab64@gmail.com>
2026-08-23 20:57:45 +0530

    Restored 99 rupee live Razorpay link

--- src/components/Sections/ApiDashboard.jsx
+++ src/components/Sections/ApiDashboard.jsx
@@ -138,7 +138,7 @@ export default function ApiDashboard({ user, themeMode = 'dark' }) {
 
   const handleUpgradeCheckout = () => {
     if (billingCycle === 'monthly') {
-      window.open('https://rzp.io/rzp/NcnHgEmB', '_blank'); 
+      window.open('https://rzp.io/rzp/jswxqVtx', '_blank'); 
     } else {
       window.open('https://rzp.io/rzp/NcnHgEmB', '_blank'); 
     }
